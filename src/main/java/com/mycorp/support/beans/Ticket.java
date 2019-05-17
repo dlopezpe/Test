fb7730266b3abc9a4701931163427275056a2338
DAMASO LOPEZ PEREZ <dlopezp@eservicios.indra.es>
2019-05-17 13:06:22 +0200

    first commit

---
+++ src/main/java/com/mycorp/support/beans/Ticket.java
@@ -0,0 +1,7 @@
+package com.mycorp.support.beans;
+
+
+
+public class Ticket {
+
+}
---
+++ src/main/java/com/mycorp/support/interfaces/MensajeriaService.java
@@ -0,0 +1,11 @@
+package com.mycorp.support.interfaces;
+
+import com.mycorp.support.CorreoElectronico;
+
+
+
+public interface MensajeriaService {
+
+    void enviar( CorreoElectronico correo );
+
+}
